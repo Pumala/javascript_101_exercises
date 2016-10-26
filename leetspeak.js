9dcaa0b83ea5b473f5450b34d3877d7925fc1a8e
Carolyn Lam <caro_lyn_@hotmail.com>
2016-10-26 11:52:17 -0400

    refactor leetspeak.js logic

--- leetspeak.js
+++ leetspeak.js
@@ -4,25 +4,45 @@ function leetspeak(phrase) {
   var new_phrase = "";
   for(var i = 0; i < phrase.length; i++) {
     var letter = phrase[i].toUpperCase();
-    if (letter === "A") {
-      new_phrase += "4";
-    } else if (letter === "E") {
-      new_phrase += "3";
-    } else if (letter === "G") {
-      new_phrase += "6";
-    } else if (letter === "L") {
-      new_phrase += "1";
-    } else if (letter === "O") {
-      new_phrase += "0";
-    } else if (letter === "S") {
-      new_phrase += "5";
-    } else if (letter === "T") {
-      new_phrase += "7";
-    } else {
-      new_phrase += letter;
-    }
+    var code = {
+        'A': '4',
+        'E': '3',
+        'G': '6',
+        'L': '1',
+        'O': '0',
+        'S': '5',
+        'T': '7'
+      }
+    new_phrase += code[letter];
   }
   return new_phrase;
 }
 
 console.log(leetspeak('Leet'));
+
+// function leetspeak(phrase) {
+//   var new_phrase = "";
+//   for(var i = 0; i < phrase.length; i++) {
+//     var letter = phrase[i].toUpperCase();
+//     if (letter === "A") {
+//       new_phrase += "4";
+//     } else if (letter === "E") {
+//       new_phrase += "3";
+//     } else if (letter === "G") {
+//       new_phrase += "6";
+//     } else if (letter === "L") {
+//       new_phrase += "1";
+//     } else if (letter === "O") {
+//       new_phrase += "0";
+//     } else if (letter === "S") {
+//       new_phrase += "5";
+//     } else if (letter === "T") {
+//       new_phrase += "7";
+//     } else {
+//       new_phrase += letter;
+//     }
+//   }
+//   return new_phrase;
+// }
+//
+// console.log(leetspeak('Leet'));
